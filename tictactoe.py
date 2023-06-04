f3caf0de6539a8a694d8b4e3651ef8b8a5a82a33
Olinizm <a.cybulka.05@gmail.com>
2023-06-04 15:42:55 +0200

    slight adjustments

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,6 @@ from colorama import Fore, Back
 import random
 
 
-
 def main():
     
     players = 0
@@ -20,14 +19,14 @@ def main():
         os.system('cls')
         if size < 3:
             print("Nieprawidłowa wielkość planszy")
-        elif size > 5:
+        elif size >= 5:
             win_req = 4
 
     gameOver = False
 
     data = []
     for i in range(size):
-        kolumna = [0 for i in range(size)]
+        kolumna = [{"player": 0, "value": 0} for i in range(size)]
         data.append(kolumna)
 
     winner = 0
@@ -40,30 +39,56 @@ def main():
             else: printRed("Gracz 2\n")
             x = int(input("Podaj wsp x: ")) - 1
             y = int(input("Podaj wsp y: ")) - 1
-            if not data[y][x]:
-                data[y][x] = player
-                gameOver = checkWin(data, x, y, player)
+            if not data[y][x]["player"]:
+                data[y][x]["player"] = player
+                gameOver = checkWin(data, x, y, player, win_req)
                 player *= -1
     else:
         player = 1
         while not gameOver:
             screenXO(data)
+            len(data)
             if player == 1: 
                 printGreen("Twoja tura\n")
                 x = int(input("Podaj wsp x: ")) - 1
                 y = int(input("Podaj wsp y: ")) - 1
             else:
-                x = random.randint(0, size - 1)
-                y = random.randint(0, size - 1)
-            if not data[y][x]:
-                data[y][x] = player
+                highest = 0
+                viable = []
+                i = 0
+                j = 0
+                # very inteligent highly advanced AI
+                for row in data:
+                    for column in row:
+                        if column["value"] > highest and not column["player"]: 
+                            highest = column["value"]
+                            viable.clear()
+                        if column["value"] == highest and not column["player"]: 
+                            viable.append([i,j])
+                        j+=1
+                    j = 0
+                    i += 1
+                index = random.randint(0,len(viable)-1)
+                y = viable[index][0]
+                x = viable[index][1]
+
+            if not data[y][x]["player"]:
+                data[y][x]["player"] = player
                 
                 gameOver = checkWin(data, x, y, player, win_req)
-                if gameOver: winner = player # jeśli checkwin zwróci prawdę, możemy zapisać aktualnego gracza jako zwyciężce
-                for list_ in data:
-                    if 0 not in list_: gameOver = True # jeśli wszystkie pola zostały zajęte, kończymy grę
-                
+                if gameOver: 
+                    winner = player # jeśli checkwin zwróci prawdę, możemy zapisać aktualnego gracza jako zwyciężce
+                    break
                 player *= -1
+
+                # jeśli wszystkie pola zostały zajęte, kończymy grę
+                gameOver = True
+                for list_ in data:
+                    if any(d["player"] == 0 for d in list_): 
+                        gameOver = False 
+                        break
+                    
+                
 
     screenXO(data)
     if winner == 1:
@@ -72,6 +97,7 @@ def main():
         printRed("Player O won!")
     else:
         printWhite("Draw!")
+        print(data)
 
 
 def printWhite(data):
@@ -121,8 +147,8 @@ def screenXO(screen):
     for i,row in enumerate(screen):
         printWhite(lines["vertical"])
         for j in row:
-            if(j>0): printGreen(" X ")
-            elif j<0: printRed(" O ")
+            if(j["player"]>0): printGreen(" X ")
+            elif j["player"]<0: printRed(" O ")
             else: printWhite("   ")
             printWhite(lines["vertical"])            
         print()
@@ -132,25 +158,34 @@ def screenXO(screen):
 
 # after the player moved checks whether it was a winning move
 def checkWin(screen, x, y, player, win_req):
+
     row = screen[y]
+
     # counts the amount of repeated symbols in the row
     count = 0
     for i in row:
-        if i == player:
+        if i["player"] == player:
             count += 1
             if count >= win_req: return True
         # resets the count when spotted a different symbol
         else:
+            # those are for my highly inteligent AI's decision making
+            if i["player"] == 0 and i["value"] < 10**count: i["value"] = 10 ** count
+            if x-count >= 0 and row[x-count]["player"] == 0 and row[x-count]["value"]< 10**count: row[x-count]["value"] = 10 ** count
             count = 0
     
     # counts the amount of repeats of a certain symbol in the column
     count = 0
     for i,row in enumerate(screen):
-        if row[x] == player:
+        if row[x]["player"] == player:
+            print(i)
             count += 1
             if count >= win_req: return True
         else:
+            if row[x]["player"] == 0 and row[x]["value"] < 10**count: row[x]["value"] = 10 ** count
+            if i-count >= 0 and screen[i-count][x]["player"] == 0 and screen[i-count][x]["value"]< 10**count: screen[i-count][x]["value"] = 10 ** count
             count = 0
+            
 
     # necessary for the bottom-left top-right diagonals
     if x > y:
@@ -161,23 +196,38 @@ def checkWin(screen, x, y, player, win_req):
 
     #checks for the streak on diagonals
     count = 0
-    for i in range(offset, size - offset):
-        if  (x >= y and screen[i - offset][i] == player) or (y > x and screen[i][i - offset] == player):
+    for i in range(size - offset):
+        if  (x >= y and screen[i][i + offset]["player"] == player) or (y > x and screen[i + offset][i]["player"] == player):
             count += 1
             if count >= win_req: return True
         else:
+            # increases the value of next and previous field
+            prev = i - count - 1
+            if x >= y:
+                if screen[i][i + offset]["player"] == 0 and screen[i][i + offset]["value"] < 10**count: screen[i][i + offset]["value"] = 10 ** count
+                if prev >= 0 and screen[prev][prev + offset]["player"] == 0 and screen[prev][prev + offset]["value"] < 10**count: screen[prev][prev + offset]["value"] = 10 ** count
+            else:
+                if screen[i + offset][i]["player"] == 0 and screen[i + offset][i]["value"] < 10**count: screen[i + offset][i]["value"] = 10 ** count
+                if prev >= 0 and screen[prev + offset][prev]["player"] == 0 and screen[prev + offset][prev]["value"] < 10**count: screen[prev + offset][prev]["value"] = 10 ** count
             count = 0
     
     count = 0
-    num_rows = x + y if x + y + 1 <= size else (size - x) + (size - y) - 2 #checks how many diagonal fields are on the axis
+    num_rows = x + y if x + y <= size - 1 else (size - x) + (size - y) - 2 #checks how many diagonal fields are on the axis
     for i in range(num_rows + 1):
-        if x + y + 1 <= size:
-            if screen[num_rows - i][i] == player: #for each field in diagonal it's y value decreases while x increases
+        if x + y <= size - 1:
+            if screen[num_rows - i][i]["player"] == player: #for each field in diagonal it's y value decreases while x increases
                 count += 1
-        elif screen[size - 1 - i][size - num_rows - 1 + i] == player: #I was writing it at 3am on coffee don't ask
+                if count >= win_req: return True
+            else:
+                if screen[num_rows - i][i]["player"] == 0 and screen[num_rows - i][i]["value"] < 10**count:  #increase the value of next field
+                    screen[num_rows - i][i]["value"] = 10 ** count
+                count = 0
+        elif screen[size - 1 - i][size - num_rows - 1 + i]["player"] == player: #I was writing it at 3am on coffee don't ask
             count += 1
             if count >= win_req: return True
         else:
+            if screen[size - 1 - i][size - num_rows - 1 + i]["player"] == player and screen[size - 1 - i][size - num_rows - 1 + i]["value"] < 10**count:
+                screen[size - 1 - i][size - num_rows - 1 + i]["value"] = 10**count
             count = 0
     return False
     
